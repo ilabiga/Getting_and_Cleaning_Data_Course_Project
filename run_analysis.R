c035eef904ebec0a3bc1583a700be264970941c8
ilabiga <ilaria.biganzoli@gmail.com>
2016-08-04 12:42:48 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,19 +2,16 @@
 library(dplyr)
 
 ## READ DATA
-## read 'features.txt' and 'activity_labels.txt'
-## inside the directory 'UCI HAR Dataset':
+## read 'features.txt' and 'activity_labels.txt' inside the directory 'UCI HAR Dataset':
 features <- read.table("C:/Users/UL16971/Downloads/Dati-R/Corso-Getting-Cleaning-Data/Project/UCI HAR Dataset/features.txt")
 activityLabels <- read.table("C:/Users/UL16971/Downloads/Dati-R/Corso-Getting-Cleaning-Data/Project/UCI HAR Dataset/activity_labels.txt")
 
-## read 'subject_train.txt', 'y_train.txt' and 'X_train.txt' 
-## inside the directory 'UCI HAR Dataset/train':
+## read 'subject_train.txt', 'y_train.txt' and 'X_train.txt' inside the directory 'UCI HAR Dataset/train':
 subjectTrain <- read.table("C:/Users/UL16971/Downloads/Dati-R/Corso-Getting-Cleaning-Data/Project/UCI HAR Dataset/train/subject_train.txt")
 yTrain <- read.table("C:/Users/UL16971/Downloads/Dati-R/Corso-Getting-Cleaning-Data/Project/UCI HAR Dataset/train/y_train.txt")
 xTrain <- read.table("C:/Users/UL16971/Downloads/Dati-R/Corso-Getting-Cleaning-Data/Project/UCI HAR Dataset/train/X_train.txt")
 
-## read 'subject_test.txt', 'y_test.txt' and 'X_test.txt' 
-## inside the directory 'UCI HAR Dataset/test':
+## read 'subject_test.txt', 'y_test.txt' and 'X_test.txt' inside the directory 'UCI HAR Dataset/test':
 subjectTest <- read.table("C:/Users/UL16971/Downloads/Dati-R/Corso-Getting-Cleaning-Data/Project/UCI HAR Dataset/test/subject_test.txt")
 yTest <- read.table("C:/Users/UL16971/Downloads/Dati-R/Corso-Getting-Cleaning-Data/Project/UCI HAR Dataset/test/y_test.txt")
 xTest <- read.table("C:/Users/UL16971/Downloads/Dati-R/Corso-Getting-Cleaning-Data/Project/UCI HAR Dataset/test/X_test.txt")
@@ -23,8 +20,7 @@ xTest <- read.table("C:/Users/UL16971/Downloads/Dati-R/Corso-Getting-Cleaning-Da
 ## append subjectTest after subjectTrain:
 subject <- rbind(subjectTrain,subjectTest)
 
-## left join between yTrain/yTest and activityLabels; the corresponding
-## activity name is associated to each experiment (= each row):
+## left join between yTrain/yTest and activityLabels; the corresponding activity name is associated to each experiment (= each row):
 yTrainActivity <- left_join(yTrain,activityLabels)
 yTestActivity <- left_join(yTest,activityLabels)
 
@@ -32,8 +28,7 @@ yTestActivity <- left_join(yTest,activityLabels)
 activity <- rbind(yTrainActivity,yTestActivity)
 
 ## 1) MERGE THE TRAINING AND TEST SETS TO CREATE ONE DATA SET
-## append xTest after xTrain and use 
-## descriptive feature names as column names
+## append xTest after xTrain and use descriptive feature names as column names
 xDataSet <- rbind(xTrain,xTest)
 colnames(xDataSet) <- features$V2
 
@@ -46,8 +41,7 @@ extraction <- mean | std
 xDataSetExtract <- xDataSet[,extraction]
 
 ## 3) USES DESCRIPTIVE ACTIVITY NAMES THE ACTIVITIES IN THE DATA SET
-## add columns containing the subject ID and the activity name
-## assicuated ti each experiment:
+## add columns containing the subject ID and the activity name assicuated ti each experiment:
 DataAll <- cbind(subject, activity[,2],xDataSetExtract)
 
 ## 4) APPROPIATELY LABELS THE DATA SET WITH DESCRIPTIVE VARIABLE NAMES
